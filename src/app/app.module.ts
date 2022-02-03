ef63d3cd3c45e450adf6a31531c4e6fad1d49851
Petar_grigorov <petar_grigorov@epam.com>
2022-02-03 17:22:55 +0200

    Added angular material to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostsModule } from './posts/posts.module';
+import { HeaderComponent } from './header/header.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, PostsModule],
+  declarations: [AppComponent, HeaderComponent],
+  imports: [BrowserModule, AppRoutingModule, PostsModule, NoopAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
