0122874962c0d37519004421afe281dc6224bdfc
Petar_grigorov <petar_grigorov@epam.com>
2022-02-03 08:25:19 +0200

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddComponent } from './posts/add/add.component';
+import { EditComponent } from './posts/edit/edit.component';
+import { IndexComponent } from './posts/index/index.component';
+import { ViewComponent } from './posts/view/view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'index', component: IndexComponent },
+  { path: 'view', component: ViewComponent },
+  { path: 'edit', component: EditComponent },
+  { path: 'add', component: AddComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './posts/index/index.component';
+import { ViewComponent } from './posts/view/view.component';
+import { EditComponent } from './posts/edit/edit.component';
+import { AddComponent } from './posts/add/add.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, IndexComponent, ViewComponent, EditComponent, AddComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
