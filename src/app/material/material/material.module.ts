26dc4b3ed6179f36e1e74c6b936913309e59cea1
Petar_grigorov <petar_grigorov@epam.com>
2022-02-04 09:48:40 +0200

    stylized header

--- src/app/material/material/material.module.ts
+++ src/app/material/material/material.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  exports: [MatButtonModule],
+  exports: [MatButtonModule, MatDividerModule],
 })
 export class MaterialModule {}
