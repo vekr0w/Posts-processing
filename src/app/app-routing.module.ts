41454b77b6e1682cc2a3b2023f37a6605fcd9da6
Petar_grigorov <petar_grigorov@epam.com>
2022-02-10 10:18:48 +0200

    added app

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PostComponent } from './posts/post/post.component';
+import { PostsComponent } from './posts/posts.component';
+
+
+
+const routes: Routes = [
+  { path: '', redirectTo: '/posts', pathMatch: 'full' },
+  { path: 'posts', component: PostsComponent },
+  { path: 'posts/add', component: PostComponent },
+  { path: 'posts/edit/:id', component: PostComponent },
+  { path: 'posts/:id', component: PostComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ src/app/posts/posts.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PostComponent } from './post/post.component';
+import { PostsComponent } from './posts.component';
+import { MaterialModule } from '../material/material/material.module';
+import { PostsTableComponent } from './posts-table/posts-table.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [PostComponent, PostsComponent, PostsTableComponent],
+  imports: [CommonModule, MaterialModule, FormsModule, ReactiveFormsModule],
+})
+export class PostsModule {}
