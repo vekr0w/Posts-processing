22f5fa618c5c240a20911b51697cdbe2b84e86c6
Petar_grigorov <petar_grigorov@epam.com>
2022-02-10 10:18:00 +0200

    removing app

--- src/app/posts/posts.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-posts',
-  templateUrl: './posts.component.html',
-  styleUrls: ['./posts.component.scss'],
-  host: { style: 'width:90%;' },
-})
-export class PostsComponent {}
